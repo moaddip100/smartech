4dc286779d47b70c17884ce8d7f25441607203d0
Ruslan <ruslanlav547@gmail.com>
2025-10-07 14:03:38 +0300

    fix categries section prev-text

--- src/components/CategoriesSection.jsx
+++ src/components/CategoriesSection.jsx
@@ -17,16 +17,28 @@ export default function CategoriesSection() {
         </div>
         <div className="cards">
           <div id="section-building" className="prev-text">
-            <h2>Text1</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam blanditiis consequuntur quod?</p>
+            <h2>B2B platform</h2>
+            <p>We welcome you to join our B2B platform. Please note 
+              that the minimum order requirement is 
+              10 Euro pallets or one 20’DC</p>
           </div>
           <div className="prev-text">
-            <h2>Text2</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam blanditiis consequuntur quod?</p>
+            <h2>DIY</h2>
+            <p>Take control. Do it yourself with SmarTech Spray Foam Insulation.</p>
+            <ul>
+              <li>Easy and fast to apply</li>
+              <li>High-efficiency</li>
+              <li>Cost effective. Easy to deliver to rural and hard-to-reach areas</li>
+              <li>Safe</li>
+              <li>Ecological</li>
+            </ul>
           </div>
           <div className="prev-text">
-            <h2>Text3</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam blanditiis consequuntur quod?</p>
+            <h2>Save energy</h2>
+            <ul>
+              <li>Reduce your heating and cooling bills with High-efficiency thermal Insulation</li>
+              <li>Do it today, and stay worry-free for the next 50 years</li>
+            </ul>
           </div>
         </div>
       </div>
